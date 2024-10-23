2aec5bb498850242410738bc09893fd4a45cc685
bMsithole19980 <bmsithole1998@gmail.com>
2024-10-23 14:41:45 +0200

    backed intergration using firebase

--- src/componets/formContainer.js
+++ src/componets/formContainer.js
@@ -2,9 +2,14 @@ import React, { useState } from 'react';
 import './form.css';
 import { ToastContainer ,toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { newDate } from 'react-datepicker/dist/date_utils';
 
 function FormContainer() {
    const [task, setTask]= useState('');
+   const [dueDate, setDueDate] = useState(newDate());
+
 
    
    const handleTask =(e)=> {
@@ -22,14 +27,23 @@ function FormContainer() {
   return (
     <div className='form_container'>
     <form onSubmit={handleTask}>
-      
-          <input
+          <label>
+            Task desc
+            <input
+          className='input-log'
           type='text'
           placeholder='Enter your task here..'
           value={task}
           onChange={(e)=>setTask(e.target.value)}
           />
-      <br/>
+            </label>
+          
+          <br/>
+          <DatePicker 
+          selected={dueDate} 
+          onChange={(date)=> setDueDate(date)}/>
+
+      
       <button type='submit'>Submit</button>
       
     </form>
